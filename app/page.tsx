b869cbcd37f293a889b6be7cf78c95fc22727afc
Al-Amin Firdows <alaminfirdows@gmail.com>
2023-01-22 18:38:43 +0600

    feat: add tailwind css

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-	return <main>Hello</main>;
+	return (
+		<div className='grid h-full w-full grid-cols-1 grid-rows-1 place-items-center'>
+			<div className='col-span-1 text-center'>
+				<h1 className='text-2xl text-white lg:text-3xl'>Al-Amin Firdows</h1>
+
+				<div className='mt-4 text-zinc-400'>
+					<ul className='inline-flex space-x-6'>
+						<li>PHP Coder</li>
+						<li>Laravel Developer</li>
+						<li>Problem Solver</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
+
