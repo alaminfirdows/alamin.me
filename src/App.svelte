9a1945e99ab76456010253f90868e5497de2ebc9
Al-Amin Firdows <alaminfirdows@gmail.com>
2023-01-22 17:33:57 +0600

    refactor: replace vanilla by svelte

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<div class="grid grid-cols-1 h-full w-full place-items-center grid-rows-1">
+    <div class="col-span-1 text-center">
+        <h1 class="text-white text-2xl lg:text-3xl">Al-Amin Firdows</h1>
+
+        <div class="text-zinc-400 mt-4">
+            <ul class="inline-flex space-x-6">
+                <li>PHP Coder</li>
+                <li>Laravel Developer</li>
+                <li>Problem Solver</li>
+            </ul>
+        </div>
+    </div>
+</div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./index.html', './src/**/*.{js,ts}'],
+	content: ['./index.html', './src/**/*.{js,ts,svelte}'],
 	theme: {
 		extend: {},
 	},
 	plugins: [],
 };
+
